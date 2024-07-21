3f2a2ad1592ab0ff2d75bb4afde0e0060cec9191
Vikas kushwaha <vikas50572kushwaha@gmail.com>
2024-07-21 11:35:28 +0530

    round off values

--- example/views.py
+++ example/views.py
@@ -14,8 +14,8 @@ def generate_quotation(request):
         data = request.POST.dict()
         data['date'] = datetime.strptime(data['date'], '%Y-%m-%d').strftime('%d-%b-%y')
         data['amount'] = float(data['quantity']) * float(data['unit_price'])
-        data['sgst'] = data['amount'] * 0.09
-        data['cgst'] = data['amount'] * 0.09
+        data['sgst'] = round(data['amount'] * 0.09,2)
+        data['cgst'] = round(data['amount'] * 0.09,2)
         data['total'] = round(data['amount'] + data['sgst'] + data['cgst'],2)
         return render(request, 'quotation.html', {'data': data})
     return render(request, 'form.html')
