83ba53b1d53448dcfc36314ad301b854302bb8fd
Vikas kushwaha <vikas50572kushwaha@gmail.com>
2024-08-12 02:36:52 +0530

    test

--- api/urls.py
+++ api/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import re_path
-from rest_framework import permissions
 
 urlpatterns = [
     path('admin/', admin.site.urls),
