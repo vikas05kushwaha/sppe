3954181d0082166e258f95cca99885316276cef8
Vikas kushwaha <vikas50572kushwaha@gmail.com>
2024-10-06 19:24:03 +0530

    title

--- example/urls.py
+++ example/urls.py
@@ -5,7 +5,7 @@ from example import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('invoice', views.newInvoice, name='invoice'),
+    path('quotation', views.newInvoice, name='quotation'),
 
     path('generate-quotation/', views.generate_quotation, name='generate_quotation'),
     ]
